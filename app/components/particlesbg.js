216af72c5453ed939f148e0061ef5b6d2b0318d3
riru12 <brilljulianrina@gmail.com>
2023-10-24 20:17:20 +0800

    particle background update

---
+++ app/components/particlesbg.js
@@ -0,0 +1,31 @@
+"use client";
+import React from 'react'
+import { useCallback } from 'react'
+import { loadFull } from 'tsparticles'
+import Particles from "react-tsparticles"
+import particlesConfig from "../config/particles-config"
+
+const ParticleBackground = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container)
+    }, []);
+
+    return (
+        <div id="particle-background">
+            <Particles 
+                id="tsparticles"
+                particlesLoaded="particlesLoaded"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={particlesConfig}
+            />
+        </div>
+    )
+}
+
+export default ParticleBackground;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Introduction from "./components/introduction"
 import AboutMe from "./components/about"
+import ParticleBackground from "./components/particlesbg"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black">
+    <main className="flex min-h-screen flex-col">
+      <ParticleBackground />
       <div className="container mx-auto px-4 py-20">
         <Introduction />
         <AboutMe />
